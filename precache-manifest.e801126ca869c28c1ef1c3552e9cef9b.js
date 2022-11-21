6cdbae6782495b9140da6a9f163b8e9d1daefc4d
Arjun E <arjunrk907@gmail.com>
2022-11-21 13:05:44 +0530

    Updates

---
+++ precache-manifest.e801126ca869c28c1ef1c3552e9cef9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "88c204f7abe5f18409a2",
+    "url": "/netflix-clone/static/css/main.cfedbf0e.chunk.css"
+  },
+  {
+    "revision": "88c204f7abe5f18409a2",
+    "url": "/netflix-clone/static/js/main.88c204f7.chunk.js"
+  },
+  {
+    "revision": "965b3c328b692e8a06d7",
+    "url": "/netflix-clone/static/js/1.965b3c32.chunk.js"
+  },
+  {
+    "revision": "0b892cc98d3bb0874451",
+    "url": "/netflix-clone/static/js/runtime~main.0b892cc9.js"
+  },
+  {
+    "revision": "59b7afb872a710647edfdea411f7ee65",
+    "url": "/netflix-clone/index.html"
+  }
+];
